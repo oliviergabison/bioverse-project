dc05f4822b86d9eab3c12eea81d19343b2950ead
Olivier Gabison <olivier@Oliviers-MBP-2.lan>
2023-12-07 21:51:47 -0500

    dist ts

---
+++ server/dist/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { createPool, sql } = require("@vercel/postgres");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const routes = require("./src/routes/index.ts");
+require("dotenv").config();
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/", routes);
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+//# sourceMappingURL=index.js.map
